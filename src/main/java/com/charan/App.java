5159fdc6012c6e1ffb21722406367e3cabafa0e2
kcharankumar167 <k.charankumar@hcltech.com>
2026-06-11 02:17:33 +0530

    Update App.java

--- src/main/java/com/charan/App.java
+++ src/main/java/com/charan/App.java
@@ -11,7 +11,7 @@ public class App {
 
     @GetMapping("/")
     public String home() {
-        return "Hello Charan! Deployment is done in Docker Hub 🚀";
+        return "Hello Charan Kumar! Deployment is done in Docker Hub 🚀";
     }
 
     public static void main(String[] args) {
